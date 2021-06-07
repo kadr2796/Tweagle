c4ebf280c90cfaf6a14239aaa44e27855e1c864a
kadr2796 <jerrylin2796@gmail.com>
2021-06-06 21:29:41 -0400

    1st Commit
    Uploaded all files

---
+++ landingpage/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Report(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    
+
+    def __str__(self):
+        return self.title + ' | by ' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('report_success')
+
+
+
+# Create your models here.
